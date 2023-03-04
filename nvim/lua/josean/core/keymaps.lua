69eeeafc4e5caa6f117dd83e925bfba4f454962a
Andev70 <anupalnath70@gmail.com>
2023-03-04 15:30:29 +0530

    best config whithout any error

---
+++ nvim/lua/josean/core/keymaps.lua
@@ -0,0 +1,86 @@
+--- Configure font for icons
+vim.g.webdevicons_enable = true
+vim.g.webdevicons_enable_nerdtree = true
+vim.g.webdevicons_enable_vimfiler = true
+vim.g.webdevicons_enable_vista = true
+vim.g.webdevicons_enable_ctrlp = true
+vim.g.webdevicons_enable_unite = true
+vim.g.webdevicons_enable_flagship_statusline = true
+vim.g.airline_extensions_tabline_enabled = 1
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_symbols = {
+  branch = '',
+  line = '',
+  readonly = '',
+  modified = '',
+  separator = '',
+  space = ' ',
+}
+--set leader key to space
+vim.g.mapleader = " "
+vim.g.python3_host_prog = '/data/data/com.termux/files/usr/bin/python3'
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- telescope git commands (not on youtube nvim video)
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
+
+-- Define a function to save the file
+function save_file()
+  vim.api.nvim_command('w')
+end
+
+-- Map Ctrl+s in insert mode to the save_file function
+vim.api.nvim_set_keymap('i', '<C-s>', '<cmd>lua save_file()<CR>', { noremap = true, silent = true })
+-- for emmet
